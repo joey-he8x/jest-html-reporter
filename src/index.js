43be9d40da4df5f9623c8f8eaa7f8ade42523858
ian.he <ian.he@centrality.ai>
2018-02-15 15:25:38 +1300

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const methods = require('./methods');
 
 class HtmlReporter {
-    constructor(globalConfig, options) {
-        this._globalConfig = globalConfig;
-        this._options = options;
-    }
+	constructor(globalConfig, options) {
+		this._globalConfig = globalConfig;
+		this._options = options;
+	}
 
-    onRunComplete(contexts, results) {
-        const destination = methods.getOutputFilepath();
-        // Generate Report
-        return methods.createReport(results, destination);
-    }
+	onRunComplete(contexts, results) {
+		const destination = methods.getOutputFilepath();
+		// Generate Report
+		return methods.createReport(results, destination);
+	}
 }
 
 module.exports = HtmlReporter;
